e81c97b7d43ace20e06c8a9af255b7c489037db2
Andrew Hoang <hoangaatrox23504@gmail.com>
2025-03-23 09:49:50 +0700

    init source

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+createApp(App).mount('#app')
